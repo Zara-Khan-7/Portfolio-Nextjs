ecd3064d4e26ac32b1c6dd10835a793d5743e13f
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-11-10 23:45:49 +0500

    portfolio

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#recentprojects" },
+    { name: "Skills", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a NextJs Personal Portfolio",
+      description: "Zara's Portfolio",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Countdown Timer",
+      des: "Integrating a Countdown Timer into Your Projects: A sleek and functional feature to track time effortlessly, adding urgency, focus, and engagement to your application.",
+      img: "/timer.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+      link: "https://github.com/Zara-Khan-7/Next-Projects.git",
+    },
+    {
+      id: 2,
+      title: "One-Page Portfolio",
+      des: "Welcome to my one-page portfolio – a concise showcase of my skills, projects, and passions. Explore my journey and achievements, all streamlined for a quick yet impactful experience.",
+      img: "/onepage-portfolio-nextjs.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/fm.svg"],
+      link: "https://github.com/Zara-Khan-7/Next-Projects.git",
+    },
+    {
+      id: 3,
+      title: "Dynamic Resume Builder",
+      des: "Create personalized, professional resumes effortlessly with a dynamic resume builder. Tailored for adaptability, it offers customizable templates, real-time updates, and intuitive design to help you stand out.",
+      img: "/dynamicresume-html.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/fm.svg"],
+      link: "https://github.com/Zara-Khan-7/Next-Projects.git",
+    },
+    {
+      id: 4,
+      title: "Figma Clone",
+      des: "I developed a Figma clone website, replicating its intuitive design and collaborative features to deliver a seamless user experience for creative projects.",
+      img: "/figma-nextjs.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/fm.svg"],
+      link: "https://github.com/Zara-Khan-7/Next-Projects.git",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "With a strong foundation in JavaScript and TypeScript, I have developed numerous projects that showcase my expertise in creating dynamic, scalable, and efficient applications. My work reflects a deep understanding of these technologies, emphasizing clean code, performance optimization, and seamless user experiences.",
+      name: "Javascript/Typescript",
+      title: "JS/TS",
+    },
+    {
+      quote:
+        "With extensive experience in Next.js, I have developed a range of projects that demonstrate my expertise in building modern, high-performance, and scalable web applications. My work highlights a deep understanding of server-side rendering, API integration, and creating seamless user experiences with cutting-edge technology.",
+      name: "Next JS",
+      title: "Next JS / React",
+    },
+    {
+      quote:
+        "With a solid expertise in HTML, I have developed numerous projects that showcase my ability to create structured, accessible, and responsive web applications. My work emphasizes clean, semantic code and a strong foundation for dynamic, user-friendly interfaces.",
+      name: "Hyper Text Markup Language",
+      title: "HTML",
+    },
+    {
+      quote:
+        "With extensive experience in custom CSS and Tailwind CSS, I have crafted numerous projects that showcase my ability to design visually appealing, responsive, and efficient user interfaces. My work highlights a deep understanding of styling principles, from writing custom code to leveraging utility-first frameworks for speed and scalability.",
+      name: "Cascase Styling Sheet",
+      title: "Css/TailwindCss",
+    },
+    {
+      quote:
+        "With a strong proficiency in Figma, I have designed and prototyped a variety of user-centric projects, emphasizing intuitive interfaces and seamless user experiences. My work demonstrates a deep understanding of design systems, wireframing, and collaboration, ensuring high-quality results from concept to execution.",
+      name: "Figma",
+      title: "UI/UX",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Typescript",
+      img: "/ts.svg",
+      nameImg: "/ts.svg",
+    },
+    {
+      id: 2,
+      name: "react",
+      img: "/re.svg",
+      nameImg: "/re.svg",
+    },
+    {
+      id: 3,
+      name: "tailwind",
+      img: "/tail.svg",
+      nameImg: "/tail.svg",
+    },
+    {
+      id: 4,
+      name: "three",
+      img: "/three.svg",
+      nameImg: "/three.svg",
+    },
+    {
+      id: 5,
+      name: "framer",
+      img: "/fm.svg",
+      nameImg: "/fm.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Phamracist",
+      desc: "As a dedicated pharmacist, I combine extensive knowledge of pharmaceuticals with a passion for patient care, ensuring safe and effective medication management while staying current with industry advancements.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "GIAIC Student",
+      desc: "I'm currently expanding my skills in web development at GIAIC, where I am learning to build dynamic, user-friendly websites and applications with modern technologies.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Web Dev Project",
+      desc: "Led the dev of a websites for a client, from initial concept to deployment.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:""
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link:''
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link:'www.linkedin.com/in/zara-yousuf-7081751b6'
+    },
+  ];
